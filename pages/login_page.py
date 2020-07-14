aada2edd52bf1e95aa4af60b55a74b274506caf7
mkasatkin <Flash20031966>
2020-07-14 16:16:29 +0400

    Initial commit

---
+++ pages/login_page.py
@@ -0,0 +1,24 @@
+from .main_page import MainPage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert self.is_element_present(*LoginPageLocators.LOGIN_PASS)
+        assert self.is_current_url()
+
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_PASS), "Login form is present"
+
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.LOGIN_PASS), "Register form is present"
